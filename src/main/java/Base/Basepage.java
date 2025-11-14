b14363463d21390b17b1b2a741ad852a19ec7f30
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-11-14 17:34:08 +0530

    New code

--- src/main/java/Base/Basepage.java
+++ src/main/java/Base/Basepage.java
@@ -7,49 +7,56 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.*;
+
+//Cucumber BDD Framework
 
 public class Basepage {
 
-    protected Basepage()
+    public Basepage()
     {
-
+        // If you want to call something before every Page Object Class call, Put your Code here");
+        // Open File, Open Data Base Connection You can write code here
     }
 
-
+    //waits
+    public void implicitWait()
+    {
+        DriverManagerTL2.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
     protected void enterInput(By by, String key)
     {
         DriverManagerTL2.getDriver().findElement(by).sendKeys(key);
     }
 
-    //Polymorphism -overloaded
-    protected  void enterInput(WebElement e,String key)
-    {
-        e.sendKeys(key);
-    }
 
-    protected void clickElement(By by)
+    public void clickElement(By by)
     {
         DriverManagerTL2.getDriver().findElement(by).click();
     }
 
-    protected WebElement getElement(By key)
+    public WebElement getElement(By key)
     {
         return DriverManagerTL2.getDriver().findElement(key);
     }
-    protected WebElement presenceOfElement(final By elementLocation) {
+    public WebElement presenceOfElement(final By elementLocation) {
         return new WebDriverWait(DriverManagerTL2.getDriver(), Duration.ofSeconds(20)).until(ExpectedConditions.presenceOfElementLocated(elementLocation));
     }
 
-    protected WebElement visibilityOfElementLocated(final By elementLocation) {
+    public WebElement visibilityOfElement(final By elementLocation) {
         return new WebDriverWait(DriverManagerTL2.getDriver(), Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfElementLocated(elementLocation));
     }
 
-    protected WebElement elementToBeClickable(final By elementIdentifier) {
-        WebElement element = new WebDriverWait(DriverManagerTL2.getDriver(), Duration.ofSeconds(20)).until(ExpectedConditions.elementToBeClickable(elementIdentifier));
-        return element;
+    public void iWaitForElementToBeVisible(WebElement loc,String url) {
+        try {
+            WebDriverWait wait = new WebDriverWait(DriverManagerTL2.getDriver(), Duration.ofSeconds(20));
+            wait.until(ExpectedConditions.visibilityOfAllElements(loc));
+            wait.until(ExpectedConditions.urlContains(url));
+        } catch (Exception e) {
+            System.out.println("Failed to Wait!: " + e.toString());
+        }
+    }
     }
 
-
-
-}
